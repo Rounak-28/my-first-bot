342174c92e50d6503aa9acf264806d91c80afce1
Rounak-28 <rounakkumbhakar2004@gmail.com>
2022-12-24 20:37:01 +0530

    first commit

---
+++ bot.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+require("./index")
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on("ready", ()=>{
+    console.log("bot is ready!")
+})
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    if (interaction.commandName === 'ping') {
+      await interaction.reply('Pong!');
+    }
+    if (interaction.commandName === 'rk') {
+        await interaction.reply('https://github.com/rounak-28');
+      }
+  });
+
+client.login(process.env.BOT_TOKEN);
